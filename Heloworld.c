d1cd3f414f97bbb6123672c1001db6ad9e08a12c
Dibyendu Pramanik <chotuu668@gmil.com>
2025-07-21 14:51:23 +0530

    first commit

---
+++ Heloworld.c
@@ -0,0 +1,17 @@
+// #include <stdio.h> directive is commented out to avoid compilation errors
+// studio input/output functions are used in the code
+// console input/output functions are used in the code optinal
+
+/**
+ * | Line                 | Purpose                                            |
+| -------------------- | -------------------------------------------------- |
+| `#include <stdio.h>` | Gives access to `printf()` and other I/O functions |
+| `int main()`         | Entry point of the program                         |
+| `printf(...)`        | Prints to the console                              |
+| `return 0;`          | Exits program with success code                    |
+ */
+#include <stdio.h>
+
+void main() {
+    printf("Hello, World!\n");
+}
