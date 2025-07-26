f7fa567650a3b13bbd0c84aff2ae054bdef55278
Dibyendu Pramanik <chotuu668@gmil.com>
2025-07-26 16:52:04 +0530

    Hello World

--- Heloworld.c
+++ Heloworld.c
@@ -3,6 +3,15 @@
 // console input/output functions are used in the code optinal
 
 /**
+ * পথম লটাইন মছিল: #include <stdio.h>,  এমট পকন মলশখমছি একটবপিশর বিলমছি।
+মদ্বিতনীয় লটাইন ফটাঘাঁকটা। পদখশত সুন্দর লটাশগ তটাই।
+ততনীয় গ লটাইন: int main()। এমটশক বিশল পমইন ফটামশন। মসি পপটাগটামগুশলটা পমইন ফটামশন পথশক কটাজ করটা শুর কশর, তটাই সিবি পপটাগটাশম
+একমট (এবিম পকবিল একমট) পমইন ফটামশন থটাকশত হয়। পমইন ফটামশশনর শুরশত মদ্বিতনীয় বিন্ধননী মদশয় শুর করশত হয় আর পশষও করশত
+হয় একমট মদ্বিতনীয় বিন্ধননী মদশয়। পশষ করটার আশগ আমম return 0;  মলশখমছি,  পসিমট পকন এখন বিল্যটাখল্যটা নটা করশলই ভটাশলটা হয়। তটাই
+আপিটাতত পতটামরটা পরশকটাশনটা পপটাগটাশম মনশচির অমশটবকু মলশখ পফলশবি:
+ * 
+ * 
+ * 
  * | Line                 | Purpose                                            |
 | -------------------- | -------------------------------------------------- |
 | `#include <stdio.h>` | Gives access to `printf()` and other I/O functions |
@@ -10,8 +19,16 @@
 | `printf(...)`        | Prints to the console                              |
 | `return 0;`          | Exits program with success code                    |
  */
+// #include <stdio.h>
+
+// void main() {
+//     printf("Hello, World!\n");
+// };
+
+
 #include <stdio.h>
 
-void main() {
-    printf("Hello, World!\n");
-}
+int main() {
+    printf("I Love My India\n");
+    return 0;
+};
